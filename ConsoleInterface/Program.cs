8fe861c04609fb151c2d749e87f2f1cca3a6eb94
tobias <tobias@refsholt.net>
2023-12-14 12:24:58 +0100

    Added missing file

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleInterface;
+using ToDoList;
+
+var database = new Database();
+var user = new User();
+var terminal = new TerminalView();
+
+while (!user.IsAuthenticated)
+{
+    user.ShowLoginPrompt();
+    user.Authenticate(database);
+}
+
+terminal.ShowMainMenu(user);
