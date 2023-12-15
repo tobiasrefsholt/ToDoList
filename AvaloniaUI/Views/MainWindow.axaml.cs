be2760768b383563f4b1eef6601b4d28ef7f3d48
tobias <tobias@refsholt.net>
2023-12-15 13:26:33 +0100

    Testing Avalonia for GUI

---
+++ AvaloniaUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AvaloniaUI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void GoToLoginPageClickHandler(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
